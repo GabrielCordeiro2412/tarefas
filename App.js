fc2b7cb07fa623eb20aa4070ba556057ae371714
GabrielCordeiro2412 <bielcordsgames.net@gmail.com>
2022-08-18 17:52:04 -0300

    AplicativoPronto

--- App.js
+++ App.js
@@ -1,20 +1,119 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  View,
+  TextInput,
+  Text,
+  TouchableOpacity,
+  FlatList
+} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function App() {
+
+  const [task, setTask] = useState("");
+  const [taskItems, setTaskItems] = useState([]);
+
+  async function handleAddTask(){
+    setTaskItems([...taskItems, task])
+    await AsyncStorage.setItem('@taskList:tasks', JSON.stringify(taskItems))
+    setTask("");
+    console.log(await AsyncStorage.getItem('@taskList:tasks'))
+  }
+
+  useEffect(() =>{
+    async function fetchData(){
+      await AsyncStorage.getItem('@taskList:tasks').then((value) => {
+        console.log(value);
+        setTaskItems(JSON.parse(value))
+    });
+      
+    }fetchData()
+  },[])
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <Text style={styles.titleText}>
+          Lista
+        </Text>
+        <View style={styles.flex}>
+          <TextInput
+            placeholder="Digite o item"
+            value={task}
+            onChangeText={text => setTask(text)}
+            underlineColorAndroid="transparent"
+            style={styles.textInputStyle}
+          />
+          <TouchableOpacity
+            onPress={handleAddTask}
+            style={styles.buttonStyle}>
+            <Text style={styles.buttonTextStyle}> Adicionar </Text>
+          </TouchableOpacity>
+        </View>
+          {
+            taskItems.map((item, index) =>{
+              return (
+                <TouchableOpacity key={index}>
+                  <Text>{item}</Text> 
+                </TouchableOpacity>
+              )
+            })
+          }
+      </View>
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 10,
+    backgroundColor: 'white',
   },
+  titleText: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    paddingVertical: 20,
+  },
+  textStyle: {
+    padding: 10,
+    textAlign: 'center'
+  },
+  buttonStyle: {
+    fontSize: 16,
+    color: 'white',
+    backgroundColor: 'purple',
+    padding: 5,
+    borderRadius: 10
+  },
+  buttonTextStyle: {
+    padding: 5,
+    color: 'white',
+    textAlign: 'center',
+  },
+  textInputStyle: {
+    textAlign: 'center',
+    height: 40,
+    borderWidth: 1,
+    borderRadius: 10,
+    width: '80%'
+  },
+  flex: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-around'
+  },
+  flex2: {
+    display: 'flex',
+    flexDirection: 'column'
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+    color: '#000',
+  }
 });
